62cfa0efab800749869b57bb1e54f97f7ad2566e
jillrizley <jillian.rizley@gmail.com>
2017-07-25 20:49:23 -0400

    add s to http

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 const config = {
   apiOrigins: {
-    development: 'http://localhost:4741',
+    development: 'https://localhost:4741',
     production: 'https://shrouded-reef-79923.herokuapp.com/'
   }
 }
