44ab61f9971af6beaed2cb71e8daa1bb00e143cb
jillrizley <jillian.rizley@gmail.com>
2017-08-07 09:05:10 -0400

    apiorigin

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -12,10 +12,10 @@
 'use strict'
 
 const config = {
-  apiOrigin: 'http://localhost:4741/'
-  // {
-    // development: 'http://localhost:4741/',
-    // production: 'https://shrouded-reef-79923.herokuapp.com'
-  // }
+  apiOrigins:
+  {
+    development: 'http://localhost:4741/',
+    production: 'https://shrouded-reef-79923.herokuapp.com'
+  }
 }
 module.exports = config
